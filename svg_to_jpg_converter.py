3b27ba87567254964c897eedf9b2b266f5d5fd77
Sukumar Varma <sukumar.varma@rap.ventures>
2021-06-06 16:12:04 +0530

    Added Zero like ramanujam

--- svg_to_jpg_converter.py
+++ svg_to_jpg_converter.py
@@ -3,10 +3,10 @@ from cairosvg import svg2png
 from tqdm import tqdm
 
 
-for num in tqdm(range(1, 100)):
+for num in tqdm(range(0, 101)):
 
     r = requests.get(f"https://progress-bar.dev/{str(num)}")
     xml = r.text
     xml = xml[xml.find('\n')+1:]
 
-    svg2png(bytestring=xml, write_to=f'progress_images/{str(num)}.jpg')
+    svg2png(bytestring=xml, write_to=f'images/progress_images/{str(num)}.jpg')
